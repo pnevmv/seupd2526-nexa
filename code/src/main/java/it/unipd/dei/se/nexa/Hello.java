b95804b62f7a0fc842906f64c093fa2730c5e1e6
Paul ARLOT <paullouisjean.arlot@studenti.unipd.it>
2026-03-23 22:14:58 +0100

    add: pom.xml

---
+++ code/src/main/java/it/unipd/dei/se/nexa/Hello.java
@@ -0,0 +1,15 @@
+package it.unipd.dei.se.nexa;
+
+/**
+ * Hello world class for testing the maven configuration.
+ */
+public class Hello {
+    /**
+     * Print Hello world!
+     * 
+     * @param args command line arguments. Ignored.
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
