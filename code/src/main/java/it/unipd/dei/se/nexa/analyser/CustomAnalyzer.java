ccc3462f002d51b556920ae1a5d7b037a5179f74
Bita Khashechian <bita.khashechian@studenti.unipd.it>
2026-03-31 12:19:48 -0200

    feat: add CustomAnalyzer with dynamic tokenizer and filter pipeline

---
+++ code/src/main/java/it/unipd/dei/se/nexa/analyser/CustomAnalyzer.java
@@ -0,0 +1,51 @@
+package it.unipd.dei.se.nexa.analyser;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.text.ParseException;
+import java.util.List;
+
+import it.unipd.dei.se.nexa.config.filters.ITokenFilterConfig;
+import it.unipd.dei.se.nexa.config.stemmer.IStemFilterConfig;
+import it.unipd.dei.se.nexa.config.config.tokenizer.ITokenizerConfig;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.Tokenizer;
+import org.apache.lucene.analysis.TokenStream;
+
+import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
+
+import it.unipd.dei.se.nexa.parser.Publication;
+import it.unipd.dei.se.nexa.parser.PublicationParser;
+
+import org.apache.lucene.analysis.LowerCaseFilter;
+
+@RequiredArgsConstructor
+public class CustomAnalyzer extends Analyzer {
+    private final List<ITokenFilterConfig> tokenFilters;
+    private final ITokenizerConfig tokenizerConfig;
+    private final IStemFilterConfig stemFilterConfig;
+
+
+    @Override
+    protected TokenStreamComponents createComponents(String fieldName) {
+
+        Tokenizer tokenizer = tokenizerConfig.toRuntime();
+
+        try {
+            TokenStream tokens = tokenizer;
+
+            for (ITokenFilterConfig tokenFilter : tokenFilters) {
+                tokens = tokenFilter.toRuntime(tokens);
+            }
+
+            tokens = stemFilterConfig.toRuntime(tokens);
+
+            return new TokenStreamComponents(tokenizer, tokens);
+
+        } catch (IOException | ParseException e) {
+            throw new IllegalStateException("Unable to add filters to TokenStream", e);
+        }
+    }
+}
