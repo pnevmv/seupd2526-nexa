c825fd097e3cf6bb0c8db873ee1d25110afc47db
Bita Khashechian <bita.khashechian@studenti.unipd.it>
2026-04-11 11:39:18 -0200

    feature(analyzer): add AnalyzerConfig interface for polymorphic JSON support

---
+++ code/src/main/java/it/unipd/dei/se/nexa/config/analyzer/AnalyzerConfig.java
@@ -0,0 +1,31 @@
+package it.unipd.dei.se.nexa.config.analyzer;
+
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import org.apache.lucene.analysis.Analyzer;
+
+/**
+ * Interface for analyzer configuration objects that define how Lucene analyzers are constructed.
+ *
+ * <p>This interface is used to support polymorphic deserialization from configuration files using Jackson.
+ * Implementing classes (e.g., {@link GenericAnalyzerConfig}, {@link FrenchAnalyzerConfig}, {@link GermanAnalyzerConfig}) specify
+ * how to construct concrete {@link Analyzer} instances at runtime.</p>
+ *
+ * <p>Jackson uses the {@code type} property in the JSON to determine which subtype to instantiate.</p>
+ *
+ * <pre>{@code
+ * {
+ *   "type": "generic",
+ *   "stopwords": ["a", "the", "of"]
+ * }
+ * }</pre>
+ */
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = GenericAnalyzerConfig.class, name = "generic"),
+//        @JsonSubTypes.Type(value = FrenchAnalyzerConfig.class, name = "french")
+})
+public interface AnalyzerConfig {
+    Analyzer toRuntime();
+}
+
