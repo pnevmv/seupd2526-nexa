d41aa74d7923b2fd79e080350a529c2ee3201573
Bita Khashechian <bita.khashechian@studenti.unipd.it>
2026-04-11 10:04:03 -0200

    feat(analyzer): StemConfig file for all the stemming methods and EnglishMinimal Stemming added

---
+++ code/src/main/java/it/unipd/dei/se/nexa/config/stemmer/StemFilterConfig.java
@@ -0,0 +1,24 @@
+package package it.unipd.dei.se.nexa.config.stemmer;
+
+import it.unipd.dei.se.nexa.config.stemmer.en.EnglishMinimalStemFilterConfig;
+//import for other languages
+import org.apache.lucene.analysis.TokenFilter;
+import org.apache.lucene.analysis.TokenStream;
+
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.JsonSubTypes;
+
+
+@JsonTypeInfo(use = JsonTypeInfo.Id.NAME, property = "type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value = EnglishMinimalStemFilterConfig.class, name = "english_minimal"),
+//        @JsonSubTypes.Type(value = FrenchMinimalStemFilterConfig.class, name = "french_minimal"),
+//        @JsonSubTypes.Type(value = FrenchLightStemFilterConfig.class, name = "french_light"),
+//        @JsonSubTypes.Type(value = FrenchSnowballStemFilterConfig.class, name = "french_snowball"),
+})
+public interface StemFilterConfig {
+    TokenFilter toRuntime(TokenStream tokenStream);
+}
+
+
